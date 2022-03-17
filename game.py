c7e39c9d980f7cb90dea99d5ec62677ab9b1a045
juliohq <JulioHenrique31501234@gmail.com>
2022-03-17 12:23:22 -0300

    remove gui for now

--- game.py
+++ game.py
@@ -1,35 +1,10 @@
-import logging
-import sys
-
-from PySide6.QtCore import QObject, QThread, Signal
-from PySide6.QtWidgets import QApplication
-
-from chess import *
+from chesspy import *
 from graphics import *
-from qt import *
 
 
-class Game(QObject):
-    quit = Signal()
-    
+class Game:
     def __init__(self):
-        super().__init__()
+        self.chess_game = ChessGame()
+        
         self.game_window = GameWindow()
-        self.game_window.destroyed.connect(self.exit)
-        self.quit.connect(lambda: self.game_window.stop())
-        
-        thread = QThread()
-        thread.started.connect(self.game_window.run)
-        self.game_window.moveToThread(thread)
-        thread.start()
-        logging.debug("Game initialized")
-        
-        self.app = QApplication(sys.argv)
-        self.window = Window()
-        logging.debug("Window initialized")
-        
-        self.app.exec()
-        self.quit.emit()
-    
-    def exit(self):
-        sys.exit()
+        self.game_window.run()
--- graphics/game_window.py
+++ graphics/game_window.py
@@ -4,6 +4,9 @@ import pygame
 from pygame import *
 from PySide6.QtCore import QObject, Signal
 
+from .board import *
+from .pieces import *
+
 
 class GameWindow(QObject):
     destroyed = Signal()
@@ -13,18 +16,39 @@ class GameWindow(QObject):
         self.width = width
         self.height = height
         self.fill = fill
-        self.screen = pygame.display.set_mode((width, height))
+        self.screen = pygame.display.set_mode((128, 131), SCALED)
         pygame.display.set_caption("Chess")
         self.running = True
+        
+        self.clock = pygame.time.Clock()
+        
+        self.pieces = pygame.sprite.Group()
+        self.board = Board2D()
+        self.visible_sprites = [
+            # self.pieces,
+            self.board,
+        ]
+        
+        logging.info("Game window initialized")
     
     def stop(self):
-        logging.debug("Game window was requested to quit")
+        logging.debug("Game window was requested to close")
         self.running = False
     
     def run(self):
         while self.running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.running = False
+                    logging.debug("Game window closed by pygame event")
+            
             self.screen.fill(self.fill)
+            
+            for sprite in self.visible_sprites:
+                sprite.run()
+            
             pygame.display.flip()
-        logging.debug("Game window quitted successfully")
-        pygame.quit()
+            self.clock.tick(60)
+        logging.debug("Game window closed successfully")
         self.destroyed.emit()
+        return
