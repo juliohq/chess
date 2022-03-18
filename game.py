a19375a34d4e3343e98f911ab921614fdf25cd03
juliohq <JulioHenrique31501234@gmail.com>
2022-03-18 20:18:32 -0300

    format game.py

--- game.py
+++ game.py
@@ -5,6 +5,6 @@ from graphics import *
 class Game:
     def __init__(self):
         self.chess_game = ChessGame()
-        
+
         self.game_window = GameWindow()
         self.game_window.run()
