d6b546fbda893b3102ae43c333f310bee61e1f02
juliohq <JulioHenrique31501234@gmail.com>
2022-03-18 20:19:07 -0300

    use default datetime format for loggings

--- run.py
+++ run.py
@@ -7,6 +7,10 @@ from game import Game
 if __name__ == "__main__":
     if not os.path.isdir("logs"):
         os.mkdir("logs")
-    
-    logging.basicConfig(filename=os.path.join("logs", time.strftime("%Y_%m_%d-%I_%M_%S.log")), level=logging.DEBUG, format="(%(levelname)s) %(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+    logging.basicConfig(
+        filename=os.path.join("logs", time.strftime("%Y_%m_%d-%I_%M_%S.log")),
+        level=logging.DEBUG,
+        format="%(asctime)s (%(levelname)s) %(name)s - %(message)s",
+    )
+
     game = Game()
